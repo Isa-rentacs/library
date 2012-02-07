269eb966bffabd49f9b0f79030fa1116cf1abc03
Isa_rentacs <jkuroda.isa@gmail.com>
2012-02-07 23:00:15 +0900

    added BIT

--- lib.cpp
+++ lib.cpp
@@ -77,3 +77,37 @@ vector<int> createpath(vector<int> &prev, int e){
         }
     }
 }
+
+/*
+ * Binary Indexed Tree
+ *
+ */
+
+class BIT{
+public:
+    int *p;
+    int num;
+    BIT(int arg){
+        p = new int[arg+1];
+        for(int i=0;i<arg+1;i++) p[i] = 0;
+        num = arg;
+    }
+    
+    //return sum of p[1] ... p[arg]
+    int sum(int arg){
+        int ret=0;
+        while(arg > 0){
+            ret += p[arg];
+            arg -= arg & -arg;
+        }
+        return ret;
+    }
+
+    //add value to index-th node
+    void add(int index, int value){
+        while(index <= num){
+            p[index] += value;
+            index += index & -index;
+        }
+    }
+};
